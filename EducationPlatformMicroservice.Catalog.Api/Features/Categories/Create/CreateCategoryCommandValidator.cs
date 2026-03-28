5fe52dacc4e6e9baddeba60f44800fa6af296384
mhuseyinbayramoglu <mhuseyinbayramoglu@gmail.com>
2026-03-28 04:15:29 +0300

    feat(validation): add reusable async FluentValidation and ValidationFilter for all microservices

---
+++ EducationPlatformMicroservice.Catalog.Api/Features/Categories/Create/CreateCategoryCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace EducationPlatformMicroservice.Catalog.Api.Features.Categories.Create
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(x => x.name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .Length(4, 50).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.");
+        }
+        
+    }
+}
--- EducationPlatformMicroservice.Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
+++ EducationPlatformMicroservice.Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using EducationPlatformMicroservice.Shared.Extensions;
+using EducationPlatformMicroservice.Shared.Filters;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,8 +10,10 @@ namespace EducationPlatformMicroservice.Catalog.Api.Features.Categories.Create
         public static RouteGroupBuilder CreateCategoryGroupItemEndpoint(this RouteGroupBuilder routeGroup)
         {
             //http://localhost:5000/api/categories/
+
             routeGroup.MapPost("/", async (CreateCategoryCommand command, IMediator mediator) =>
-             (await mediator.Send(command)).ToGenericResult());
+             (await mediator.Send(command)).ToGenericResult())
+                .AddEndpointFilter<ValidationFilter<CreateCategoryCommand>>();
 
             return routeGroup;
         }
